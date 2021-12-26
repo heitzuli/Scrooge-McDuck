2f4de7b87de50245df3f1cad6fd6308821476de3
heidi <heidi.poels@gmail.com>
2021-12-26 13:55:20 +0200

    Get price data from API and find and show the LDS.

--- bitcoinTrends.js
+++ bitcoinTrends.js
@@ -1,3 +1,6 @@
+// One hour in seconds
+const ONE_HOUR = 60 * 60;
+
 //Fetch data in JSON format
 function getJSON(url) {
     // Fetches from given URL
@@ -8,22 +11,18 @@ function getJSON(url) {
         })
 }
 
-function search() {
-    //console.log(document.getElementById("startDate").value);
-    //console.log(document.getElementById("endDate").value);
-
+async function searchLDS() {
+    // TODO: Verify both start and end date exist and are valid dates
     const startDate = document.getElementById("startDate").value;
-    const endDate = document.getElementById("endDate").value;
-    const found = getJSON(config.baseURL + `?vs_currency=eur&from=${startDate}&to=${endDate}`);
-    console.log(found);
+    const endDate = +document.getElementById("endDate").value + ONE_HOUR;
+    const data = await getJSON(config.baseURL + `?vs_currency=eur&from=${startDate}&to=${endDate}`);
 
-    //console.log(Math.max());
-
-    /*const searchTerm = document.getElementById('search').value;
-    const foundIt = smileys.hasOwnProperty(searchTerm);
-    if (foundIt) {
-        displayImg(smileys[searchTerm], 'emoji');
-    } else {
-        deleteImg('emoji');
-    }*/
+    // We're assuming every day has at least one price at midnight
+    // Prices array looks like [date, price]. Aka the price is taken from prices array, place 1.
+    const pricesWithoutDate = data.prices.map(array => array[1]);
+    // Call function to find longest downward trend
+    const longestDownwardTrend = lengthOfLongestDownwardTrend(pricesWithoutDate);
+    // Display longest downward trend
+    const element = document.getElementById("lds");
+    element.innerHTML = `Longest bearish trend is ${longestDownwardTrend} days.`;
 }
